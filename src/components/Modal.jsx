a34a7237777d95b5ce5426c5ba8b96d4b947566d
Aishwarya <atom.aishwarya@gmail.com>
2025-05-24 16:59:28 +0400

    modal change

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,43 +1,4 @@
-// import React from "react";
-// import { Spinner } from "react-bootstrap";
-// const Modal = ({ user, loading, onClose }) => {
-//   if (loading) return (
-//     <div className="flex justify-center items-center h-screen">
-//       <Spinner animation="border" variant="danger" />
-//     </div>
-//   );
 
-//   return (
-//     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-//       <div className="bg-white p-6 rounded shadow-lg w-full max-w-md">
-//         <h2 className="text-xl font-bold mb-4">User Details</h2>
-//         <p>
-//           <strong>ID:</strong> {user.useridId}
-//         </p>
-//         <p>
-//           <strong>Name:</strong> {user.name}
-//         </p>
-//         <p>
-//           <strong>Email:</strong> {user.email}
-//         </p>
-//         <p>
-//           <strong>Role:</strong> {user.role}
-//         </p>
-//         <p>
-//           <strong>Phone:</strong> {user.phone}
-//         </p>
-//         <button
-//           onClick={onClose}
-//           className="mt-4 bg-gray-300 px-4 py-2 rounded hover:bg-gray-400"
-//         >
-//           Close
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Modal;
 import React from "react";
 import { Spinner } from "react-bootstrap";
 
